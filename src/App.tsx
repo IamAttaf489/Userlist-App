1be508408e64f6883528e99764b11d6a54408f34
IamAttaf489 <attaf.dev.02@gmail.com>
2024-12-02 14:52:42 +0500

    Removed Redundant File

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import UserList from './components/UserList';
-import UserDetails from './components/UserDetails';
+import UserList from "./components/UserList";
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<UserList />} />
-        <Route path="/user/:id" element={<UserDetails />} />
       </Routes>
     </Router>
   );
